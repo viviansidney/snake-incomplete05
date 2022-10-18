ab7b4e9e7a7822d2dc25cba5880ec7a3f41eadcc
studentwdd230 <studentwdd230.github.io>
2022-10-18 11:36:04 +0800

    first commit

---
+++ snake-incomplete/snake/action.py
@@ -0,0 +1,5 @@
+class Action:
+    
+    def execute(self, cast, script):
+        
+        pass
---
+++ snake-incomplete/snake/move_actors_action.py
@@ -0,0 +1,8 @@
+from game.scripting.action import Action
+
+
+#TODO: Implementing MoveActorsAction class
+class MoveActionAcvtion(Action):
+    
+    def executed(self, cast, script): 
+        pass
