193bcecee5f830e9c4259a8c2d729e8f093b4359
studentwdd230 <studentwdd230.github.io>
2022-10-22 15:22:13 +0800

    first commit

--- snake-incomplete/snake/game/casting/snake.py
+++ snake-incomplete/snake/game/casting/snake.py
@@ -1,69 +1,118 @@
 import constants
-from game.casting.actor import Actor
+from game.shared.color import Color
 from game.shared.point import Point
 
 
-class Snake(Actor):
-    """
-    A long limbless reptile.
+class Actor:
+    """A visible, moveable thing that participates in the game. 
     
-    The responsibility of Snake is to move itself.
+    The responsibility of Actor is to keep track of its appearance, position and velocity in 2d 
+    space.
 
     Attributes:
-        _points (int): The number of points the food is worth.
+        _text (string): The text to display
+        _font_size (int): The font size to use.
+        _color (Color): The color of the text.
+        _position (Point): The screen coordinates.
+        _velocity (Point): The speed and direction.
     """
+
     def __init__(self):
-        super().__init__()
-        self._segments = []
-        self._prepare_body()
+        """Constructs a new Actor."""
+        self._text = ""
+        self._font_size = 15
+        self._color = Color(255, 255, 255)
+        self._position = Point(0, 0)
+        self._velocity = Point(0, 0)
 
-    def get_segments(self):
-        return self._segments
+    def get_color(self):
+        """Gets the actor's color as a tuple of three ints (r, g, b).
+        
+        Returns:
+            Color: The actor's text color.
+        """
+        return self._color
 
-    def move_next(self):
-        # move all segments
-        for segment in self._segments:
-            segment.move_next()
-        # update velocities
-        for i in range(len(self._segments) - 1, 0, -1):
-            trailing = self._segments[i]
-            previous = self._segments[i - 1]
-            velocity = previous.get_velocity()
-            trailing.set_velocity(velocity)
+    def get_font_size(self):
+        """Gets the actor's font size.
+        
+        Returns:
+            Point: The actor's font size.
+        """
+        return self._font_size
 
-    def get_head(self):
-        return self._segments[0]
-
-    def grow_tail(self, number_of_segments):
-        for i in range(number_of_segments):
-            tail = self._segments[-1]
-            velocity = tail.get_velocity()
-            offset = velocity.reverse()
-            position = tail.get_position().add(offset)
-            
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text("#")
-            segment.set_color(constants.GREEN)
-            self._segments.append(segment)
-
-    def turn_head(self, velocity):
-        self._segments[0].set_velocity(velocity)
+    def get_position(self):
+        """Gets the actor's position in 2d space.
+        
+        Returns:
+            Point: The actor's position in 2d space.
+        """
+        return self._position
     
-    def _prepare_body(self):
-        x = int(constants.MAX_X / 2)
-        y = int(constants.MAX_Y / 2)
+    def get_text(self):
+        """Gets the actor's textual representation.
+        
+        Returns:
+            string: The actor's textual representation.
+        """
+        return self._text
 
-        for i in range(constants.SNAKE_LENGTH):
-            position = Point(x - i * constants.CELL_SIZE, y)
-            velocity = Point(1 * constants.CELL_SIZE, 0)
-            text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.GREEN
-            
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text(text)
-            segment.set_color(color)
-            self._segments.append(segment)
+    def get_velocity(self):
+        """Gets the actor's speed and direction.
+        
+        Returns:
+            Point: The actor's speed and direction.
+        """
+        return self._velocity
+    
+    def move_next(self):
+        """Moves the actor to its next position according to its velocity. Will wrap the position 
+        from one side of the screen to the other when it reaches the given maximum x and y values.
+        
+        Args:
+            max_x (int): The maximum x value.
+            max_y (int): The maximum y value.
+        """
+        x = (self._position.get_x() + self._velocity.get_x()) % constants.MAX_X
+        y = (self._position.get_y() + self._velocity.get_y()) % constants.MAX_Y
+        self._position = Point(x, y)
+
+    def set_color(self, color):
+        """Updates the color to the given one.
+        
+        Args:
+            color (Color): The given color.
+        """
+        self._color = color
+
+    def set_position(self, position):
+        """Updates the position to the given one.
+        
+        Args:
+            position (Point): The given position.
+        """
+        self._position = position
+    
+    def set_font_size(self, font_size):
+        """Updates the font size to the given one.
+        
+        Args:
+            font_size (int): The given font size.
+        """
+        self._font_size = font_size
+    
+    def set_text(self, text):
+        """Updates the text to the given value.
+        
+        Args:
+            text (string): The given value.
+        """
+        self._text = text
+
+    def set_velocity(self, velocity):
+        """Updates the velocity to the given one.
+        
+        Args:
+            velocity (Point): The given velocity.
+        """
+        self._velocity = velocity
